e002b4ab6bb3e59fbe71186d2c142ccabe36eb69
zhangdi <1258956799@qq.com>
2023-04-01 22:13:24 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: zhangdi 1258956799@qq.com
+ * @Date: 2023-04-01 22:10:12
+ * @LastEditors: zhangdi 1258956799@qq.com
+ * @LastEditTime: 2023-04-01 22:10:56
+ * @FilePath: /my-project/tailwind.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+// tailwind.config.js
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
