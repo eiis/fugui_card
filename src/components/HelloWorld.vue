695555792329f9246390f9f6730980e800fdc977
zhangdi <1258956799@qq.com>
2023-04-01 23:26:17 +0800

    添加别名@导致打包相对路径报错的问题

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
  * @Author: zhangdi 1258956799@qq.com
  * @Date: 2023-04-01 22:08:38
  * @LastEditors: zhangdi 1258956799@qq.com
- * @LastEditTime: 2023-04-01 22:58:01
+ * @LastEditTime: 2023-04-01 23:24:51
  * @FilePath: /my-project/src/components/HelloWorld.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,7 +12,7 @@
 <template>
   <div class="p-6 max-w-sm mx-auto bg-red-700 hover:bg-white rounded-xl shadow-md flex items-center space-x-4 dark:bg-gray-800">
   <div class="flex-shrink-0">
-    <img class="h-12 w-12 " src="src/assets/vue.svg" alt="ChitChat Logo">
+    <img class="h-12 w-12 " src="@/assets/vue.svg" alt="ChitChat Logo">
   </div>
   <div class="group ">
     <div class="text-xl font-medium text-black group-hover:text-gray-900">Vite+Vue</div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/*
+ * @Author: zhangdi 1258956799@qq.com
+ * @Date: 2023-04-01 22:08:38
+ * @LastEditors: zhangdi 1258956799@qq.com
+ * @LastEditTime: 2023-04-01 23:21:57
+ * @FilePath: /my-project/vite.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      // 将 `@` 解析为 `/src` 目录
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
