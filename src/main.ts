0f0594159d2f00e923f507a65dc3ccaa337fca40
zhangdi <1258956799@qq.com>
2023-06-28 23:20:11 +0800

    :sparkles: 添加mkcert去掉烦人的 https 证书错误

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: zhangdi 1258956799@qq.com
  * @Date: 2023-04-01 22:08:38
  * @LastEditors: zhangdi 1258956799@qq.com
- * @LastEditTime: 2023-04-01 22:18:40
+ * @LastEditTime: 2023-04-02 06:12:51
  * @FilePath: /my-project/src/main.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
