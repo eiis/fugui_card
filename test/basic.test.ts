8cd73a7f16e0592aa455ea9611c2332912027b59
zhangdi <1258956799@qq.com>
2023-08-18 10:10:18 +0800

    add:添加test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -3,11 +3,40 @@ import { describe, expect, it } from 'vitest'
 
 import TheCounter from '../src/components/TheCounter.vue'
 
-describe('tests', () => {
-  it('should works', () => {
+describe('TheCounter.vue', () => {
+  // 测试组件是否渲染
+  it('should render', () => {
+    // 创建一个TheCounter组件，并传入initial属性
     const wrapper = mount(TheCounter, { props: { initial: 10 } })
-    console.log(wrapper)
-    // expect(1 + 1).toMatchSnapshot()
-    expect(1 + 1).toEqual(2)
+    // 断言wrapper中text()的值是否包含'10'
+    expect(wrapper.text()).toContain('10')
+    // 断言wrapper中html()的值是否包含快照
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  // 测试组件是否可以接受点击事件
+  it('should be interactive', async () => {
+    // 创建一个TheCounter组件，并传入initial属性
+    const wrapper = mount(TheCounter, { props: { initial: 0 } })
+    // 断言wrapper中text()的值是否包含'0'
+    expect(wrapper.text()).toContain('0')
+
+    // 断言wrapper中.inc组件是否存在
+    expect(wrapper.find('.inc').exists()).toBe(true)
+
+    // 断言wrapper中.dec组件是否存在
+    expect(wrapper.find('.dec').exists()).toBe(true)
+
+    // 触发.inc组件的点击事件
+    await wrapper.get('.inc').trigger('click')
+
+    // 断言wrapper中text()的值是否包含'1'
+    expect(wrapper.text()).toContain('1')
+
+    // 触发.dec组件的点击事件
+    await wrapper.get('.dec').trigger('click')
+
+    // 断言wrapper中text()的值是否包含'0'
+    expect(wrapper.text()).toContain('0')
   })
 })
