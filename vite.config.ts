377f3085ebce0f7e437bd5a76379e3a2bb00afcf
zhangdi <1258956799@qq.com>
2023-09-04 13:03:00 +0800

    chore:add log

--- vite.config.ts
+++ vite.config.ts
@@ -46,7 +46,7 @@ export default defineConfig(({ command, mode }) => {
   // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
   const env = loadEnv(mode, process.cwd(), '')
 
-  // console.log('process.env.BASE_ENV', process.env)
+  console.log('process.env.BASE_ENV', env.VITE_APP_TITLE)
 
   return {
     // 剔除 options_api
